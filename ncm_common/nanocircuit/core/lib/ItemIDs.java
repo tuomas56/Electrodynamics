aaeb1303dac2baaca61cb632c878326d572ac8e7
Dylan Miller <dmillerw@gmail.com>
2013-04-27 09:17:11 -0700

    Moved item ids out of block id range

--- ncm_common/nanocircuit/core/lib/ItemIDs.java
+++ ncm_common/nanocircuit/core/lib/ItemIDs.java
@@ -4,10 +4,10 @@ public class ItemIDs {
 
 	/* itemComponent */
 	public static int ITEM_COMPONENT_ID;
-	public static final int ITEM_COMPONENT_DEFAULT_ID = 1500;
+	public static final int ITEM_COMPONENT_DEFAULT_ID = 5100;
 	
 	/* itemPCB */
 	public static int ITEM_PCB_ID;
-	public static final int ITEM_PCB_DEFAULT_ID = 1501;
+	public static final int ITEM_PCB_DEFAULT_ID = 5101;
 	
 }
