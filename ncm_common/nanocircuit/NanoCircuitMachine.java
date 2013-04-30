709c93dee7248ea097c4c2bddde73bd2b12ee1d2
Dylan Miller <dmillerw@gmail.com>
2013-04-30 12:16:29 -0700

    NC has basic techne model support
    Kind of hacked on, pulled from RG's pull request to Forge.

--- ncm_common/nanocircuit/NanoCircuitMachine.java
+++ ncm_common/nanocircuit/NanoCircuitMachine.java
@@ -2,6 +2,8 @@ package nanocircuit;
 
 import nanocircuit.core.lib.ModInfo;
 import nanocircuit.machine.core.CommonProxy;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.techne.TechneModelLoader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -10,6 +12,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid = "NC|Machine", name = "NanoCircuit - Machine", version = ModInfo.VERSION, dependencies = "required-after:NC|Core;required-after:NC|World")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
@@ -21,7 +24,9 @@ public class NanoCircuitMachine {
 
 	@PreInit
 	public void preInit(FMLPreInitializationEvent event) {
-
+		if (event.getSide() == Side.CLIENT) {
+			AdvancedModelLoader.registerModelHandler(new TechneModelLoader());
+		}
 	}
 
 	@Init
