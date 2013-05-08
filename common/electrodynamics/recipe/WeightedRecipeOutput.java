b76fc0ab6a01fa874577102c2f05624c16388492
Dylan Miller <dmillerw@gmail.com>
2013-05-08 05:43:37 -0700

    Added some overflow constructors to WeightedRecipeOutput + more recipes

--- common/electrodynamics/recipe/WeightedRecipeOutput.java
+++ common/electrodynamics/recipe/WeightedRecipeOutput.java
@@ -3,28 +3,38 @@ package electrodynamics.recipe;
 import java.util.Random;
 
 import net.minecraft.item.ItemStack;
+import electrodynamics.lib.item.Component;
+import electrodynamics.lib.item.Dust;
 
 public class WeightedRecipeOutput {
 
 	public ItemStack output;
 	
-	public int chance;
+	public float chance;
 	
 	/**
 	 * Allows for machines to have a random chance of producing items
 	 * @param output ItemStack to output
-	 * @param chance Chance of output. Value is an int from 0 - 100
+	 * @param chance Chance of output. Value is an int from 0 - 1
 	 */
-	public WeightedRecipeOutput(ItemStack output, int chance) {
+	public WeightedRecipeOutput(ItemStack output, float chance) {
 		this.output = output;
 		this.chance = chance;
 		
 		if (chance < 0) chance = 0;
-		if (chance > 100) chance = 100;
+		if (chance > 1) chance = 1;
+	}
+	
+	public WeightedRecipeOutput(Dust output, float chance) {
+		this(output.toItemStack(), chance);
+	}
+	
+	public WeightedRecipeOutput(Component output, float chance) {
+		this(output.toItemStack(), chance);
 	}
 	
 	public boolean willOutput(Random random) {
-		return (random.nextInt(100) <= this.chance);
+		return (random.nextFloat() <= this.chance);
 	}
 	
 }
