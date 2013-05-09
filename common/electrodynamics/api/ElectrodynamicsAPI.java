de6b482d393a368e8563b02dcb5108226ccfa523
Dylan Miller <dmillerw@gmail.com>
2013-05-09 00:53:46 -0700

    Fixed API

--- common/electrodynamics/api/ElectrodynamicsAPI.java
+++ common/electrodynamics/api/ElectrodynamicsAPI.java
@@ -1,12 +1,11 @@
 package electrodynamics.api;
 
 import cpw.mods.fml.common.Loader;
-import electrodynamics.api.crafting.ICraftingManager;
 
 public class ElectrodynamicsAPI {
 
-	public static ICraftingManager getCraftingManager() {
-		return (ICraftingManager) Loader.instance().getIndexedModList().get("ED").getMod();
+	public static IEDApi getAPIInterface() {
+		return (IEDApi) Loader.instance().getIndexedModList().get("ED").getMod();
 	}
 	
 }
