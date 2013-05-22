c78a01acb3e4f4311ed4b60d45e7165f0b94302b
Xhamolk <amilcar.menjivar11@gmail.com>
2013-05-21 23:47:47 -0600

    Final (?) retouches before releasing.  - Sintering Furnace pattern.  - "Invalid/Valid Structure" message no longer duplicated.

--- common/electrodynamics/mbs/structure/SinteringFurnace.java
+++ common/electrodynamics/mbs/structure/SinteringFurnace.java
@@ -111,7 +111,7 @@ public class SinteringFurnace extends MultiBlockStructure {
 				{ 'w', 'w', 'w' },
 				{ 'g', 'v', 'g' },
 				{ 'g', 'v', 'g' },
-				{ 'w', 'w', 'w' },
+				{ 'q', 'v', 'q' },
 				{ 'w', 'w', 'w' },
 				{ '-', '-', '-' }
 		} );
