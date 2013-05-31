288a58cd80996d1a285f91de72ab62b6bf28b09b
Dylan Miller <dmillerw@gmail.com>
2013-05-30 22:05:42 -0700

    Sledge hammer now does extra damage + knockback

--- common/electrodynamics/item/hammer/ItemSledgeHammer.java
+++ common/electrodynamics/item/hammer/ItemSledgeHammer.java
@@ -1,7 +1,11 @@
 package electrodynamics.item.hammer;
 
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
 import electrodynamics.lib.core.ModInfo;
 
 public class ItemSledgeHammer extends ItemHammer {
@@ -22,4 +26,15 @@ public class ItemSledgeHammer extends ItemHammer {
 		texture = register.registerIcon(ModInfo.ICON_PREFIX + "tool/sledgeHammer");
 	}
 
+	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+		final int KNOCKBACK = 2;
+		
+		entity.addVelocity((double)(-MathHelper.sin(player.rotationYaw * (float)Math.PI / 180.0F) * (float)KNOCKBACK * 0.5F), 0.2D, (double)(MathHelper.cos(player.rotationYaw * (float)Math.PI / 180.0F) * (float)KNOCKBACK * 0.5F));
+		return false;
+	}
+	
+	public int getDamageVsEntity(Entity entity) {
+		return 10;
+	}
+	
 }
