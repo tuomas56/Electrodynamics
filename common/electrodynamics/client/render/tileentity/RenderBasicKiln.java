17a38996c7638880d10126a339fc224053ef5333
Xhamolk <amilcar.menjivar11@gmail.com>
2013-06-03 23:07:04 -0600

    Networking functionality and more rendering for the Basic Kiln.

--- common/electrodynamics/client/render/tileentity/RenderBasicKiln.java
+++ common/electrodynamics/client/render/tileentity/RenderBasicKiln.java
@@ -1,21 +1,29 @@
 package electrodynamics.client.render.tileentity;
 
 import electrodynamics.client.model.ModelBasicKiln;
+import electrodynamics.client.model.ModelMetalTray;
 import electrodynamics.lib.client.Models;
 import electrodynamics.tileentity.TileEntityBasicKiln;
 import electrodynamics.tileentity.TileEntityMachine;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 import org.lwjgl.opengl.GL11;
 
 
 public class RenderBasicKiln extends TileEntitySpecialRenderer {
 
 	private ModelBasicKiln modelKiln;
+	private ModelMetalTray modelMetalTray;
 
 	public RenderBasicKiln() {
 		modelKiln = new ModelBasicKiln();
+		modelMetalTray = new ModelMetalTray();
 	}
 
 	@Override
@@ -46,16 +54,84 @@ public class RenderBasicKiln extends TileEntitySpecialRenderer {
 			}
 		}
 
+		TileEntityBasicKiln kiln = (TileEntityBasicKiln) tile;
+
 		Minecraft.getMinecraft().renderEngine.bindTexture( Models.TEX_BASIC_KILN );
-		modelKiln.rotateDoor( ((TileEntityBasicKiln) tile).doorAngle );
+		modelKiln.rotateDoor( kiln.doorAngle );
 		modelKiln.renderAll( 0.0625F );
 
 		// todo: render fire and particles
 
-		// todo: render tray
+		if( kiln.trayInventory != null ) { // render tray
+			renderTray( kiln.worldObj, kiln.trayInventory.inventory );
+		}
 
 		GL11.glEnable( GL11.GL_LIGHTING );
 		GL11.glPopMatrix();
+	}
+
+	public void renderTray(World world, ItemStack[] inv) {
+		GL11.glTranslated( 0, 0.1f, 0 );
+		GL11.glRotatef( 90, 0, 1, 0 );
+
+		GL11.glScaled( 0.75f, 0.75f, 0.75f );
+		Minecraft.getMinecraft().renderEngine.bindTexture( Models.TEX_METAL_TRAY );
+		modelMetalTray.render( 0.0625F );
+
+		GL11.glRotatef( 270, 0, 1, 0 );
+
+		if( inv != null && inv.length > 0 ) {
+			GL11.glTranslated( -.11, 1.35, .165 );
+			GL11.glScaled( .4, .4, .4 );
+
+			for( int i = 0; i < inv.length; i++ ) {
+				ItemStack stack = inv[i];
+				if( stack == null )
+					continue;
+
+				if( i != 0 ) {
+					GL11.glTranslated( .28, 0, 0 );
+				}
+
+				if( i == 3 || i == 6 ) {
+					GL11.glTranslated( -.84, 0, -.38 );
+				}
+
+				if( !(stack.getItem() instanceof ItemBlock) ) {
+					GL11.glPushMatrix();
+					GL11.glScaled( .8, .8, .8 );
+					GL11.glRotatef( 90, 1, 0, 0 );
+					GL11.glTranslated( 0, -.24, -.26 );
+
+					renderItem( world, stack );
+
+					GL11.glPopMatrix();
+				} else {
+					GL11.glPushMatrix();
+					GL11.glRotatef( 180, 0, 0, 1 );
+					GL11.glTranslated( 0, -.2, 0 );
+
+					renderItem( world, stack );
+
+					GL11.glPopMatrix();
+				}
+			}
+		}
+	}
+
+	public void renderItem(World world, ItemStack stack) {
+		if (stack != null) {
+			//Incredibly hackish, but better than essentially writing out a copy of the EntityItem renderer
+			boolean fancy = Minecraft.getMinecraft().gameSettings.fancyGraphics;
+			Minecraft.getMinecraft().gameSettings.fancyGraphics = true;
+
+			EntityItem entityitem = new EntityItem(world, 0.0D, 0.0D, 0.0D, stack);
+			entityitem.getEntityItem().stackSize = 1;
+			entityitem.hoverStart = 0.0F;
+			RenderManager.instance.renderEntityWithPosYaw(entityitem, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
+
+			Minecraft.getMinecraft().gameSettings.fancyGraphics = fancy;
+		}
 	}
 
 }
--- common/electrodynamics/tileentity/TileEntityBasicKiln.java
+++ common/electrodynamics/tileentity/TileEntityBasicKiln.java
@@ -1,20 +1,187 @@
 package electrodynamics.tileentity;
 
+import electrodynamics.inventory.InventoryItem;
+import electrodynamics.item.EDItems;
+import electrodynamics.util.InventoryUtil;
+import electrodynamics.util.ItemUtil;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.ForgeDirection;
+
+import java.util.Random;
 
 public class TileEntityBasicKiln extends TileEntityMachine {
 
 	public final float ROTATIONAL_MAX = 1.5F;
 	public float doorAngle = 0;
+	public boolean open = false;
+
+	public int fuelLevel = 0;
+	public boolean burning = false;
+	public int currentCookTime;
+	public int totalCookTime;
+
+	public InventoryItem trayInventory;
+
+
+	@Override
+	public void updateEntityClient() {
+		if( open && doorAngle <= ROTATIONAL_MAX ) {
+			doorAngle += 0.2F;
+		} else if( !open && doorAngle > 0 ) {
+			doorAngle -= 0.2F;
+		}
+
+		if( doorAngle < 0 )
+			doorAngle = 0;
+		if( doorAngle > ROTATIONAL_MAX )
+			doorAngle = ROTATIONAL_MAX;
+	}
+
+	// updateEntityServer
+
+	@Override
+	public void onBlockBreak() {
+		InventoryUtil.ejectItem( worldObj, xCoord, yCoord, zCoord, ForgeDirection.UP, trayInventory.parent.copy(), new Random() );
+	}
+
+	@Override
+	public void onBlockActivated(EntityPlayer player) {
+		if( player.isSneaking() ) { // open and close the door.
+			this.open = !this.open;
+			sendOpenUpdate();
+			return;
+		}
+
+		ItemStack playerHeldItem = player.getHeldItem();
+		if( playerHeldItem != null ) {
+			if( ItemUtil.getFuelValue( playerHeldItem ) > 0 ) { // refuel
+				this.fuelLevel += ItemUtil.getFuelValue( playerHeldItem );
+				--playerHeldItem.stackSize;
+				if( playerHeldItem.itemID == Item.bucketLava.itemID ) { // return the empty bucket when using lava as fuel.
+					ItemStack bucket = new ItemStack( Item.bucketEmpty, 1 );
+					if( playerHeldItem.stackSize == 0 )
+						player.setCurrentItemOrArmor( 0, bucket );
+					else
+						player.inventory.addItemStackToInventory( bucket );
+				}
+				this.burning = true;
+				sendBurningUpdate();
+				((EntityPlayerMP) player).updateHeldItem();
+
+			} else if( trayInventory == null && playerHeldItem.getItem() == EDItems.itemTray ) { // place tray
+
+				this.trayInventory = new InventoryItem( 9, playerHeldItem.copy() );
+				--playerHeldItem.stackSize;
+
+				sendTrayUpdate();
+				((EntityPlayerMP) player).updateHeldItem();
+			}
+
+		} else if( this.trayInventory != null ) { // remove tray
+			player.setCurrentItemOrArmor( 0, trayInventory.parent.copy() );
+
+			this.trayInventory = null;
+			this.currentCookTime = 0;
+			this.totalCookTime = 0;
+
+			sendTrayUpdate();
+			((EntityPlayerMP) player).updateHeldItem();
+		}
+	}
+
+
+	// ----- NBT and sync packets -----
 
 	@Override
 	public void writeToNBT(NBTTagCompound tag) {
 		super.writeToNBT( tag );
+		tag.setBoolean( "open", open );
+		tag.setInteger( "fuelLevel", fuelLevel );
+		if( this.trayInventory != null ) {
+			this.trayInventory.writeToNBT( tag );
+		}
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound tag) {
 		super.readFromNBT( tag );
+		open = tag.getBoolean( "open" );
+		this.fuelLevel = tag.getInteger( "fuelLevel" );
+		this.burning = this.fuelLevel > 0;
+		if( tag.hasKey( "Items" ) ) {
+			this.trayInventory = new InventoryItem( 9, new ItemStack( EDItems.itemTray ) );
+			this.trayInventory.readFromNBT( tag );
+		}
+	}
+
+	@Override
+	public void getDescriptionForClient(NBTTagCompound nbt) {
+		super.getDescriptionForClient( nbt );
+		nbt.setBoolean( "open", open );
+		nbt.setBoolean( "burning", burning );
+		nbt.setInteger( "fuelLevel", fuelLevel );
+		if( this.trayInventory != null ) {
+			this.trayInventory.writeToNBT( nbt );
+		}
+	}
+
+	@Override
+	public void onDescriptionPacket(NBTTagCompound nbt) { // load
+		super.onDescriptionPacket( nbt );
+		this.open = nbt.getBoolean( "open" );
+		this.burning = nbt.getBoolean( "burning" );
+		this.fuelLevel = nbt.getInteger( "fuelLevel" );
+		if( nbt.hasKey( "Items" ) ) {
+			this.trayInventory = new InventoryItem( 9, new ItemStack( EDItems.itemTray ) );
+			this.trayInventory.readFromNBT( nbt );
+		} else {
+			this.trayInventory = null;
+		}
+	}
+
+	@Override
+	public void onUpdatePacket(NBTTagCompound nbt) { // update
+		super.onUpdatePacket( nbt );
+		if( nbt.hasKey( "open" ) ) {
+			this.open = nbt.getBoolean( "open" );
+		}
+		if( nbt.hasKey( "burning" ) ) {
+			this.burning = nbt.getBoolean( "burning" );
+		}
+		if( nbt.hasKey( "noTray" ) ) {
+			this.trayInventory = null;
+		}
+		if( nbt.hasKey( "Items" ) ) {
+			this.trayInventory = new InventoryItem( 9, new ItemStack( EDItems.itemTray ) );
+			this.trayInventory.readFromNBT( nbt );
+		}
+	}
+
+	private void sendOpenUpdate() {
+		NBTTagCompound nbt = new NBTTagCompound();
+		nbt.setBoolean( "open", this.open );
+		sendUpdatePacket( nbt );
+	}
+
+	private void sendBurningUpdate() {
+		NBTTagCompound nbt = new NBTTagCompound();
+		nbt.setBoolean( "burning", this.burning );
+
+		sendUpdatePacket( nbt );
+	}
+
+	private void sendTrayUpdate() {
+		NBTTagCompound nbt = new NBTTagCompound();
+		if( this.trayInventory != null )
+			this.trayInventory.writeToNBT( nbt );
+		else
+			nbt.setBoolean( "noTray", true );
+
+		sendUpdatePacket( nbt );
 	}
 
 }
