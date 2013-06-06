d94351d5ae3e12e8bf5c0adf03fda374c904fe51
Dylan Miller <dmillerw@gmail.com>
2013-06-06 14:34:00 -0700

    Tweaked magnetic pull module to not accept items behind blocks
    Also some tweaking to reduce lightning spam

--- common/electrodynamics/item/elmag/logic/ElMagLogicMagneticPull.java
+++ common/electrodynamics/item/elmag/logic/ElMagLogicMagneticPull.java
@@ -1,7 +1,13 @@
 package electrodynamics.item.elmag.logic;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.Packet28EntityVelocity;
+import net.minecraft.world.World;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.PacketDispatcher;
 import cpw.mods.fml.relauncher.Side;
@@ -9,14 +15,11 @@ import electrodynamics.api.tool.IElMagLogic;
 import electrodynamics.configuration.ConfigurationSettings;
 import electrodynamics.network.PacketTypeHandler;
 import electrodynamics.network.packet.PacketLightningFX;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.network.packet.Packet28EntityVelocity;
-import net.minecraft.world.World;
 
 public class ElMagLogicMagneticPull implements IElMagLogic {
 
+	public ArrayList<EntityItem> zappedItems = new ArrayList<EntityItem>();
+	
 	public int cooldown = 0;
 	
 	@SuppressWarnings("unchecked")
@@ -27,29 +30,31 @@ public class ElMagLogicMagneticPull implements IElMagLogic {
 		List<EntityItem> nearbyItems = world.getEntitiesWithinAABB(EntityItem.class, player.boundingBox.expand(ConfigurationSettings.MAGNETIC_RANGE, ConfigurationSettings.MAGNETIC_RANGE, ConfigurationSettings.MAGNETIC_RANGE));
 
 		for (EntityItem item : nearbyItems) {
-			if (item != null) {
-				if (item.delayBeforeCanPickup > 0) {
-					return;
-				}
+			if (player.canEntityBeSeen(item)) {
+				if (item != null) {
+					if (item.delayBeforeCanPickup > 0) {
+						return;
+					}
 
-				double d0 = 8.0D;
-				double d1 = (player.posX - item.posX) / d0;
-				double d2 = (player.posY + (double) player.getEyeHeight() - item.posY) / d0;
-				double d3 = (player.posZ - item.posZ) / d0;
-				double d4 = Math.sqrt(d1 * d1 + d2 * d2 + d3 * d3);
-				double d5 = 1.0D - d4;
+					double d0 = 8.0D;
+					double d1 = (player.posX - item.posX) / d0;
+					double d2 = (player.posY + (double) player.getEyeHeight() - item.posY) / d0;
+					double d3 = (player.posZ - item.posZ) / d0;
+					double d4 = Math.sqrt(d1 * d1 + d2 * d2 + d3 * d3);
+					double d5 = 1.0D - d4;
 
-				if (d5 > 0.0D) {
-					d5 *= d5;
-					item.motionX += d1 / d4 * d5 * ConfigurationSettings.MAGNETIC_ATTRACTION_SPEED;
-					item.motionY += d2 / d4 * d5 * ConfigurationSettings.MAGNETIC_ATTRACTION_SPEED;
-					item.motionZ += d3 / d4 * d5 * ConfigurationSettings.MAGNETIC_ATTRACTION_SPEED;
+					if (d5 > 0.0D) {
+						d5 *= d5;
+						item.motionX += d1 / d4 * d5 * ConfigurationSettings.MAGNETIC_ATTRACTION_SPEED;
+						item.motionY += d2 / d4 * d5 * ConfigurationSettings.MAGNETIC_ATTRACTION_SPEED;
+						item.motionZ += d3 / d4 * d5 * ConfigurationSettings.MAGNETIC_ATTRACTION_SPEED;
 
-					PacketDispatcher.sendPacketToAllInDimension(new Packet28EntityVelocity(item), world.provider.dimensionId);
-					
-					if (cooldown == 0) {
-						PacketDispatcher.sendPacketToAllAround(player.posX, player.posY, player.posZ, 50D, player.worldObj.provider.dimensionId, PacketTypeHandler.fillPacket(new PacketLightningFX(player.posX, player.posY + 1.5, player.posZ, item.posX, item.posY, item.posZ, 1)));
-						cooldown = 20;
+						PacketDispatcher.sendPacketToAllInDimension(new Packet28EntityVelocity(item), world.provider.dimensionId);
+
+						if (!zappedItems.contains(item)) {
+							PacketDispatcher.sendPacketToAllAround(player.posX, player.posY, player.posZ, 50D, player.worldObj.provider.dimensionId, PacketTypeHandler.fillPacket(new PacketLightningFX(player.posX, player.posY + 1.5, player.posZ, item.posX, item.posY, item.posZ, 1)));
+							zappedItems.add(item);
+						}
 					}
 				}
 			}
