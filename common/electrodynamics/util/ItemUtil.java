0efc498d6cf7b38570e37cd517a7caa89455ffc0
Xhamolk <amilcar.menjivar11@gmail.com>
2013-06-07 23:40:41 -0600

    Added a some methods to ItemUtil for dealing with List<ItemStack>

--- common/electrodynamics/util/ItemUtil.java
+++ common/electrodynamics/util/ItemUtil.java
@@ -26,19 +26,8 @@ public class ItemUtil {
 			if( current == null )
 				continue;
 
-			int index;
-			boolean contained = false;
-			for(index = 0; index < result.size(); index++) {
-				ItemStack s = result.get( index );
-				if( current.itemID == s.itemID && current.getItemDamage() == s.getItemDamage() ) {
-					if( ignoreNBT || ItemStack.areItemStackTagsEqual( current, s ) ) {
-						contained = true;
-						break;
-					}
-				}
-			}
-
-			if( contained ) {
+			int index = getIndexOf( current, result, ignoreNBT );
+			if( index != -1 ) {
 				result.get( index ).stackSize += current.stackSize;
 			} else {
 				result.add( current.copy() );
@@ -47,4 +36,111 @@ public class ItemUtil {
 		return result;
 	}
 
+	public static boolean isItemInList(ItemStack item, List<ItemStack> list, boolean ignoreNBT) {
+		if( item == null )
+			return list.contains( null ); // I won't deal with this..
+		for( ItemStack current : list ) {
+			if( current == null )
+				continue;
+			if( areItemStacksSameItem( current, item, ignoreNBT ) )
+				return true;
+		}
+		return false;
+	}
+
+	public static boolean areItemStacksSameItem(ItemStack item1, ItemStack item2, boolean ignoreNBT) {
+		if( item1 == null || item2 == null )
+			return item1 == item2;
+
+		return item1.itemID == item2.itemID && item1.getItemDamage() == item2.getItemDamage()
+				&& (ignoreNBT || ItemStack.areItemStackTagsEqual( item1, item2 ));
+	}
+
+	public static int getIndexOf(ItemStack item, List<ItemStack> list, boolean ignoreNBT) {
+		for( int index = 0; index < list.size(); index++ ) {
+			ItemStack s = list.get( index );
+			if( areItemStacksSameItem( item, s, ignoreNBT ) ) {
+				return index;
+			}
+		}
+		return -1;
+	}
+
+	// removes the exact amount of the items on the first list, from the second list.
+	public static List<ItemStack> removeItemsFromList(List<ItemStack> items, List<ItemStack> from) {
+		List<ItemStack> result = new ArrayList<ItemStack>();
+		items = copyList( trimItemsList( items, false ) );
+		from = copyList( from );
+
+		for( ItemStack current : from ) {
+			if( current == null )
+				continue;
+			int index = getIndexOf( current, items, false );
+			if( index == -1 ) {
+				result.add( current );
+			} else {
+				ItemStack stack = items.get( index );
+
+				if( current.stackSize >= stack.stackSize ) {
+					current.stackSize -= stack.stackSize;
+					stack.stackSize = 0;
+					if( current.stackSize > 0 )
+						result.add( current );
+				} else {
+					stack.stackSize -= current.stackSize;
+				}
+
+				if( stack.stackSize < 0 )
+					stack.stackSize = 0;
+			}
+		}
+		return result;
+	}
+
+	public static void removeItemsFromArray(List<ItemStack> items, ItemStack[] from) {
+		items = copyList( trimItemsList( items, false ) );
+		for( int i = 0; i<from.length; i++) {
+			ItemStack current = from[i];
+			if( current == null )
+				continue;
+			int index = getIndexOf( current, items, false );
+			if( index != -1 ) {
+				ItemStack stack = items.get( index );
+
+				if( current.stackSize >= stack.stackSize ) {
+					current.stackSize -= stack.stackSize;
+					stack.stackSize = 0;
+				} else {
+					stack.stackSize -= current.stackSize;
+					current.stackSize = 0;
+				}
+
+				if( stack.stackSize < 0 )
+					stack.stackSize = 0;
+				if( current.stackSize <= 0 )
+					from[i] = null;
+			}
+
+		}
+	}
+
+	public static List<ItemStack> removeListFromList(List<ItemStack> remove, List<ItemStack> from) {
+		List<ItemStack> result = new ArrayList<ItemStack>();
+		for( ItemStack current : from ) {
+			if( !isItemInList( current, remove, false ) )
+				result.add( current );
+		}
+		return result;
+	}
+
+	public static List<ItemStack> copyList(List<ItemStack> list) {
+		List<ItemStack> result = new ArrayList<ItemStack>();
+		for( ItemStack item : list ) {
+			if( item == null )
+				result.add( null );
+			else
+				result.add( item.copy() );
+		}
+		return result;
+	}
 }
