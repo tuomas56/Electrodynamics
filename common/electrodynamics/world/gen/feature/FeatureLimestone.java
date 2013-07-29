57232b5e65fd6c9af8248c7476bb8cd1510ea9a0
loophole <loophole@morpheus.net>
2013-07-28 23:11:03 -0500

    Worldgen changes: limestone can now be generated. Probably will need to be tuned down.

--- common/electrodynamics/world/gen/feature/FeatureLimestone.java
+++ common/electrodynamics/world/gen/feature/FeatureLimestone.java
@@ -3,6 +3,7 @@ package electrodynamics.world.gen.feature;
 import java.util.Random;
 
 import electrodynamics.lib.block.Decorative;
+import electrodynamics.lib.block.BlockIDs;
 import electrodynamics.world.gen.feature.FeatureHandler.FeatureType;
 
 import net.minecraft.block.Block;
@@ -12,54 +13,51 @@ import net.minecraftforge.common.Configuration;
 
 public class FeatureLimestone extends FeatureBase {
 
-	public int radius;
-	
-	public FeatureLimestone(String name) {
-		super(name);
-	}
+  public int radius;
 
-	@Override
-	public void generateFeature(Random random, int chunkX, int chunkZ, World world, boolean newGeneration) {
-		if (world.getWorldInfo().getTerrainType() == WorldType.FLAT) {
-			return;
-		}
-		
-		int x = chunkX * 16;
-		int y = random.nextInt(64);
-		int z = chunkZ * 16;
+  public FeatureLimestone(String name) {
+    super(name);
+  }
 
-		if (world.getBlockId(x, y, z) != 0) {
-			return;
-		} else {
-			int l = random.nextInt(this.radius - 2) + 2;
-			byte b0 = 2;
+  @Override
+    public void generateFeature(Random random, int chunkX, int chunkZ, World world, boolean newGeneration) {
+      if (world.getWorldInfo().getTerrainType() == WorldType.FLAT) {
+        return;
+      }
 
-			for (int i1 = x - l; i1 <= x + l; ++i1) {
-				for (int j1 = z - l; j1 <= z + l; ++j1) {
-					int k1 = i1 - x;
-					int l1 = j1 - z;
+      int x = chunkX * 16 + 8;
+      int z = chunkZ * 16 + 8;
+      for(int y=5; y <= 64; y++){
 
-					if (k1 * k1 + l1 * l1 <= l * l) {
-						for (int i2 = y - b0; i2 <= y + b0; ++i2) {
-							if (i2 > 0) {
-								if ((i1 >= chunkX * 16 && i1 < (chunkX * 16) + 16) && (j1 >= chunkZ * 16 && j1 < (chunkZ * 16) + 16)) {
-									int j2 = world.getBlockId(i1, i2, j1);
+        if (world.getBlockId(x, y, z) != 0) {
+          continue;
+        } else {
+          int l = random.nextInt(this.radius - 2) + 2;
+          byte b0 = 2;
 
-									if (j2 == Block.dirt.blockID || j2 == Block.stone.blockID) {
-										world.setBlock(i1, i2, j1, Decorative.LIMESTONE.ordinal(), 0, 2);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+          for (int i1 = x - l; i1 <= x + l; ++i1) {
+            for (int j1 = z - l; j1 <= z + l; ++j1) {
+              int k1 = i1 - x;
+              int l1 = j1 - z;
 
-	@Override
-	public void handleConfig(Configuration config) {
-		this.radius = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "radius", 25).getInt(25);
-	}
-	
+              if (k1 * k1 + l1 * l1 <= l * l) {
+                if ((i1 >= chunkX * 16 && i1 < (chunkX * 16) + 16) && (j1 >= chunkZ * 16 && j1 < (chunkZ * 16) + 16)) {
+                  int j2 = world.getBlockId(i1, y, j1);
+
+                  if (j2 == Block.dirt.blockID || j2 == Block.stone.blockID) {
+                    world.setBlock(i1, y, j1, BlockIDs.BLOCK_DECORATIVE_ID, 0, Decorative.LIMESTONE.ordinal());
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+  @Override
+    public void handleConfig(Configuration config) {
+      this.radius = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "radius", 25).getInt(25);
+    }
+
 }
